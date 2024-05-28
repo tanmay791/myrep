ca84b94a6f7c05daeb114b9c8499ed3e32cfffff
Tanmay <tanmayjuneja34@gmail.com>
2024-05-27 21:40:49 -0400

    Add Additional_scrpit.py

---
+++ additional_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# additional_script.py
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet(name))
+
+
+# In[ ]:
+
+
+
+
